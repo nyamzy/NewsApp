ab79affdd9069ba798322b09cc180e9885320375
nyamzy <john.nyamweya@student.moringaschool.com>
2021-12-13 12:58:07 +0300

    create the configuration classes

---
+++ app/config.py
@@ -0,0 +1,21 @@
+from re import DEBUG
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    '''
+    DEBUG = True
+    
