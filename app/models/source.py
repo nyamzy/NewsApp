382c2ee6d3e69d4304b7a499d17ff19357acdac2
nyamzy <john.nyamweya@student.moringaschool.com>
2021-12-13 16:59:26 +0300

    create a model class for news sources

---
+++ app/models/source.py
@@ -0,0 +1,9 @@
+class Source:
+    '''
+    Source class to define the News Source Objects
+    '''
+    def __init__(self, id, name, url):
+        self.id = id
+        self.name = name
+        self.url = url
+        
