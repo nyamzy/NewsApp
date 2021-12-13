b29886a9a808505bca5696027f1a6cb4e1dfaa2d
nyamzy <john.nyamweya@student.moringaschool.com>
2021-12-13 18:03:15 +0300

    create function for getting sources

--- app/request.py
+++ app/request.py
@@ -1,4 +1,28 @@
 from app import app
+import urllib.request, json
+from .models import source
+
+Source = source.Source
 
 #Getting the api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the news source url
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_sources():
+
+    get_sources_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_sources(source_results_list)
+
+    return source_results
+
