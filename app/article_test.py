ba070c0b121598046d515543d4772ba7a332e6d8
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-13 15:40:49 +0300

    create tests for articles model

---
+++ app/article_test.py
@@ -0,0 +1,13 @@
+import unittest
+from .models import article
+Article = article.Article
+
+class ArticleTest(unittest.TestCase):
+    def setUp(self):
+        self.new_article = Article('cnn', 'Serial killer on the loose', 'There is a serial killer on the loose in city streets', 'http://www.cnn.com/news/world-us-canada-59658661','https://ichef.bbci.co.uk/news/1024/branded_news/14ED7/production/_117891758_gettyimages-111178852.jpg','2021-12-14T18:52:19.4576902Z')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article, Article))
+
+if __name__ == '__main__':
+    unittest.main()
