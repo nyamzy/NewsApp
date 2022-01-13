ba831b810e6b208906a825b3c1d2f2bdc8945395
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-13 15:43:24 +0300

    update url for getting articles

--- app/config.py
+++ app/config.py
@@ -5,7 +5,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_ARTICLES_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
 class ProdConfig(Config):
     '''
