86157af3b533359b029a2b25653724f91fa82569
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-14 11:52:22 +0300

    current deployment

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_ARTICLES_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
